e7be7edbf5d216b1c77af06b9d676383a495a6fd
Sarnika K <sharu022kemp@gmail.com>
2024-04-26 18:58:54 +0530

    6.36.39

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -6,29 +6,41 @@ export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState("");
   const [loading, setLoading] = useState(false);
   const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState({});
 
-
-  async function handleSubmit(event){
+  async function handleSubmit(event) {
     event.preventDefault();
     try {
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`);
-        const data = await res.json()
-        if(data?.data?.recipes){
-            setRecipeList(data.data.recipes);
-            setLoading(false)
-            setSearchParam("")
-        }
-        console.log(data);
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
+      const data = await res.json();
+      if (data?.data?.recipes) {
+        setRecipeList(data.data.recipes);
+        setLoading(false);
+        setSearchParam("");
+      }
+      console.log(data);
     } catch (error) {
-        console.log(error);
-        setLoading(false)
-        setSearchParam("")
+      console.log(error);
+      setLoading(false);
+      setSearchParam("");
     }
   }
 
   console.log(loading, recipeList);
   return (
-    <GlobalContext.Provider value={{ searchParam, loading, recipeList, setSearchParam, handleSubmit }}>
+    <GlobalContext.Provider
+      value={{
+        searchParam,
+        loading,
+        recipeList,
+        setSearchParam,
+        handleSubmit,
+        recipeDetailsData,
+        setRecipeDetailsData,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
