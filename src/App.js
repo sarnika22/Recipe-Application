1f485c11a3fbe6251f9b404f267d97e5c8f5cf38
Sarnika K <sharu022kemp@gmail.com>
2024-04-26 15:09:16 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import Favourites from "./pages/favourites";
+import Details from "./pages/details";
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favourites" element={<Favourites />}></Route>
+          <Route path="/recipe-item/:id" element={<Details />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
